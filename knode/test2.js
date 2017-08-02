9e2e0c7cd6e1322148ac5dfcaca58ca34610fa94
nxycdl <nxycdl@163.com>
2017-08-02 16:10:47 +0800

    commit

--- knode/test2.js
+++ knode/test2.js
@@ -13,7 +13,7 @@ var netPull = new CronJob(
 );
 
 var netPullEthOur = new CronJob(
-    '0 0 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * * *',
+    '0 0 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * * * ',
     function () {
         console.log('netPullEthOur',new Date())
     },

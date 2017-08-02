9a99072cb3906bdd0b64a0d6a58a87d0a359a2f0
nxycdl <nxycdl@163.com>
2017-08-02 20:19:39 +0800

    commit

--- knode/test2.js
+++ knode/test2.js
@@ -1,28 +1,9 @@
 /**
  * Created by dl on 2017-05-30.
  */
-var CronJob = require('cron').CronJob;
-var netPull = new CronJob(
-    '0 */3 * * * *',
-    // '*/5 * * * * *',
-    function () {
-        console.log('netPull',new Date())
-    },
-    false,
-    "Asia/Shanghai"
-);
-
-var netPullEthOur = new CronJob(
-    '0 0 0/1 * * * * ',
-    function () {
-        console.log('netPullEthOur',new Date())
-    },
-    false,
-    "Asia/Shanghai"
-);
-
-netPullEthOur.start();
-netPull.start();
-
+var loadsh = require('lodash');
+var _=loadsh;
+var a = '11';
+console.log(_.isEmpty(a));
 
 

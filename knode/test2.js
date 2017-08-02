d7d2a8ef51f85fd5c2117b15bdfebff940755de1
nxycdl <nxycdl@163.com>
2017-08-02 15:44:36 +0800

    commit

--- knode/test2.js
+++ knode/test2.js
@@ -1,36 +1,28 @@
 /**
  * Created by dl on 2017-05-30.
  */
-var moment = require('moment');
-var data ={};
-data.currentTime =      moment.unix('1501556733738').format('YYYY-MM-DD HH:mm:ss');
-data.x1 =                 moment.unix('1501556734000').format('YYYY-MM-DD HH:mm:ss');
-data.x2 =                 moment.unix('1501556734').format('YYYY-MM-DD HH:mm:ss');
-var d = new Date();
-console.log(d.getHours() + ':'+ d.getMinutes() + ':' +d.getSeconds());
-var a=[1,2,3,4,5,6];
-a.shift();
-a = []
-var underLine=[];
+var CronJob = require('cron').CronJob;
+var netPull = new CronJob(
+    '0 */3 * * * *',
+    // '*/5 * * * * *',
+    function () {
+        console.log('netPull',new Date())
+    },
+    false,
+    "Asia/Shanghai"
+);
 
-console.log(underLine);
-var cnt = '';
-var key1 = '';
-console.log('xxx',underLine.length);
-if (underLine.length >= 4){
-    cnt = underLine[0] + underLine[1];
-    key1 = underLine[2] + underLine[3];
-}else if (underLine.length == 3){
-    cnt = underLine[0] + underLine[1];
-    key1 = underLine[2]
-}else if(underLine.length == 2){
-    console.log('xxx',key1);
-    cnt = underLine[0] + underLine[1];
-    key1 = '';
-}else if(underLine.length == 1){
-    cnt = underLine[0] ;
-    key1 = '';
-};
-console.log(cnt)
-console.log(key1)
+var netPullEthOur = new CronJob(
+    '0 0 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * * *',
+    function () {
+        console.log('netPullEthOur',new Date())
+    },
+    false,
+    "Asia/Shanghai"
+);
+
+netPullEthOur.start();
+netPull.start();
+
+
 

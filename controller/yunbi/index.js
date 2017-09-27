063000503a0af09bea2ccf4cac807f8859faac61
nxycdl <nxycdl@163.com>
2017-09-28 02:24:14 +0800

    commit

--- controller/yunbi/index.js
+++ controller/yunbi/index.js
@@ -233,7 +233,6 @@ module.exports = {
         var key1 = 't' + market.toUpperCase() + 'BTC';
         var key = 'BTC_' + market.toUpperCase();
         var bitfinexData = yield this.bitfinexService.getTickers(keybtc + ',' + key1);
-        console.log('1235', bitfinexData.body);
         bitfinexData = JSON.parse(bitfinexData.body);
         var bitfinexBtc ={
             symbol :bitfinexData[0][0],

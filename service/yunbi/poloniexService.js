c9e8a823ec6252cc6e868fc376d7d15712bea730
nxycdl <nxycdl@163.com>
2017-09-28 02:17:51 +0800

    commit

--- service/yunbi/poloniexService.js
+++ service/yunbi/poloniexService.js
@@ -5,7 +5,7 @@ module.exports = function (self) {
     return {
         getPloniexTickers: function*() {
 
-            var url = 'http://47.74.32.135/poloniex/returnTicker';
+            var url = 'http://45.77.70.81:3000/poloniex/returnTicker';
             var options = {};
             options.url = url;
             options.method = 'GET';
